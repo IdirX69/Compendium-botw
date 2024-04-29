9f0e6ee8184c6b50b96946f68f9f29c2e000d479
IdirX69 <adil.idirr@gmail.com>
2024-04-30 00:08:25 +0200

    burger menu mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            Header
-        </div>
-    );
+  return (
+    <div className="header-container">
+      <div className="menuToggle">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul>
+          <li>Creatures</li>
+          <li>Equipment</li>
+          <li>Materials</li>
+          <li>Monsters</li>
+          <li>Treasure</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
