3d587b0af12925f958bc7953391d1e9e9c4f628d
IdirX69 <adil.idirr@gmail.com>
2024-05-02 21:51:12 +0200

    icon info

--- src/components/ModalInfo.tsx
+++ src/components/ModalInfo.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ModalInfo = ({ modalInfo, setModal }) => {
+  const [dropModal, setDropModal] = useState(false);
   return (
     <div className="modal-info-container">
       <span onClick={() => setModal(false)}>x</span>
 
       <h4>{modalInfo.name}</h4>
-      <div>
+      <div className="info-container">
         <img src={modalInfo.image} alt="image" />
         <img src="./korogu.png" alt="" className="korogu-img" />
+        <div className="icon-container">
+          <img src="./heart.png" alt="" className="heart-img" />
+          <ul onClick={() => setDropModal(!dropModal)}>
+            Drop items
+            {dropModal && modalInfo.drops?.map((drop) => <li>{drop}</li>)}
+          </ul>
+        </div>
         <p>{modalInfo.description}</p>
       </div>
     </div>
