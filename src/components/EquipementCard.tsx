30a72ff96c68a2facac29ccd084bfa18830fd549
IdirX69 <adil.idirr@gmail.com>
2024-05-07 22:58:45 +0200

    equipement page

---
+++ src/components/EquipementCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EquipementCard = () => {
+  return <div></div>;
+};
+
+export default EquipementCard;
--- src/pages/Equipements.tsx
+++ src/pages/Equipements.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 
 const Equipements = () => {
+  const [equipement, setEquipement] = useState([]);
+  const [search, setSearch] = useState("");
+  const [modal, setModal] = useState(false);
+  const [modalInfo, setModalInfo] = useState([]);
+  console.log(equipement);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://botw-compendium.herokuapp.com/api/v3/compendium/category/equipement"
+      )
+      .then((res) => setEquipement(res.data.data));
+  }, [search]);
   return (
     <div>
       <div className="creatures-container">
