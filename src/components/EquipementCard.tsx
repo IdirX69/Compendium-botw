96a5a4f21d1333c9d7925c4a37c4aff77c33a26e
IdirX69 <adil.idirr@gmail.com>
2024-05-07 23:11:43 +0200

    equipement card test

--- src/components/EquipementCard.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EquipementCard = () => {
-  return <div></div>;
-};
-
-export default EquipementCard;
--- src/pages/Equipements.tsx
+++ src/pages/Equipements.tsx
@@ -1,19 +1,19 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import MonsterCard from "../components/MonsterCard";
 
 const Equipments = () => {
-  const [equipment, setEquipment] = useState([]);
+  const [equipments, setEquipments] = useState([]);
   const [search, setSearch] = useState("");
   const [modal, setModal] = useState(false);
   const [modalInfo, setModalInfo] = useState([]);
-  console.log(equipment);
 
   useEffect(() => {
     axios
       .get(
         "https://botw-compendium.herokuapp.com/api/v3/compendium/category/equipment"
       )
-      .then((res) => setEquipment(res.data.data));
+      .then((res) => setEquipments(res.data.data));
   }, [search]);
   return (
     <div>
@@ -24,6 +24,11 @@ const Equipments = () => {
           placeholder="Type to search"
           onChange={(e) => setSearch(e.target.value)}
         />
+        <div className="monsters-list">
+          {equipments.map((object) => (
+            <MonsterCard monster={object} />
+          ))}
+        </div>
       </div>
     </div>
   );
